b58a6196084aa75a3c70fce7576f2a8264d9e3a8
WONDongin <bangsa100@naver.com>
2026-05-07 22:54:40 +0900

    feat: 공통 코드 엔티티 구조 개선

--- backend/src/main/java/com/project/crm/domain/code/dto/CodeResponseDto.java
+++ backend/src/main/java/com/project/crm/domain/code/dto/CodeResponseDto.java
@@ -1,4 +1,12 @@
 package com.project.crm.domain.code.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class CodeResponseDto {
+
+    private String code;
+    private String name;
 }
